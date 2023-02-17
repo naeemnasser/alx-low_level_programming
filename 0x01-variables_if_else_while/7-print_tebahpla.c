831ff509dd9f084a7f0c340fc9c28162404daba4
naeemnasser <naeem@gmail.com>
2023-02-17 15:07:29 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - main block
+ * Description: prints all singls int digitnum oof base 10
+ * starting form 0
+ * Return: 0
+ */
+int main(void)
+{
+
+	char c = '2';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
