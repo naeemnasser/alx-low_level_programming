51849f0fb63c7b69857d357c24efd95ef8bc83d9
naeemnasser <naeem@gmail.com>
2023-02-17 07:26:21 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  *main - A program that print value of size of varible
-
  *Return: Always 0
  */
 int main(void)
@@ -11,10 +10,10 @@ int num;
 long int age;
 long long int nums;
 float doc;
-printf("size of char: %i byte(s)\n", sizeof(number));
-printf("size of int: %i byte(s)\n", sizeof(num));
-printf("size of long int: %i byte(s)\n", sizeof(age));
-printf("size of long long int: %i byte(s)\n", sizeof(nums));
-printf("size of float: %i byte(s)\n", sizeof(doc));
+printf("size of char: %zu byte(s)\n", sizeof(number));
+printf("size of int: %zu byte(s)\n", sizeof(num));
+printf("size of long int: %zu byte(s)\n", sizeof(age));
+printf("size of long long int: %zu byte(s)\n", sizeof(nums));
+printf("size of float: %zu byte(s)\n", sizeof(doc));
 return (0);
 }
