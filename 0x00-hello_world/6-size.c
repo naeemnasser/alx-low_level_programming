80b3e9b45ae692436007b3801fdaf3dc60f5a278
naeemnasser <naeem@gmail.com>
2023-02-16 19:13:00 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *Return: Always 0
+ */
+int main(void)
+{
+printf("size of char: %i byte(s)\n", sizeof(char));
+printf("size of int: %i byte(s)\n", sizeof(int));
+printf("size of long int: %i byte(s)\n", sizeof(long int));
+printf("size of long long int: %i byte(s)\n", sizeof(long long int));
+printf("size of float: %i byte(s)\n", sizeof(float));
+return (0);
+}
